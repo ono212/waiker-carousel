24ce849f852392ea128241f7efaf8652f3ab6e2d
ono212 <qkrwjdals302@naver.com>
2024-10-08 15:57:28 +0900

    refactor: 슬라이드 간격을 setTimeout으로 조절하도록 수정
    - 기존에 setInterval을 사용하던 방식에서 setTimeout을 사용하도록 변경
    - 트랜지션이벤트가 끝나면 setTimeout으로 delay 시작

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,8 +1,13 @@
 import { addDragAndTouchEventHandlers } from '../utils/addDragAndTouchEventHandlers.js';
 
-const DEFAULT_SLIDE_SPEED = 55_000; // 기본 슬라이드 속도
+const DEFAULT_TRANSITION_SPEED = 3_000; // 기본 슬라이드 속도
 
-export function Carousel({ slides, carouselSlide, delay, transitionSpeed }) {
+export function Carousel({
+  slides,
+  carouselSlide,
+  delay,
+  transitionSpeed = DEFAULT_TRANSITION_SPEED,
+}) {
   const totalSlides = slides.length;
   const totalSlidesWithClone = totalSlides + 1; // 복제된 슬라이드를 포함한 총 슬라이드 개수
 
@@ -11,6 +16,10 @@ export function Carousel({ slides, carouselSlide, delay, transitionSpeed }) {
 
   this.currentSlide = 0;
   this.isPlaying = true;
+  this.delay = delay;
+
+  let autoSlideTimeout = null;
+  let lastSlideTimeout = null;
 
   const indicatorButtons = [];
 
@@ -23,32 +32,49 @@ export function Carousel({ slides, carouselSlide, delay, transitionSpeed }) {
 
     // 마지막 슬라이드에서 첫 번째 슬라이드로 부드럽게 전환
     if (this.currentSlide === totalSlides) {
-      setTimeout(() => {
+      lastSlideTimeout = setTimeout(() => {
         this.currentSlide = 0;
         carouselSlide.style.transition = 'none';
         carouselSlide.style.transform = `translateX(0%)`;
         this.updateIndicator();
-      }, delay - 300);
+      }, transitionSpeed);
     }
   };
 
   // 슬라이드 전환
   this.setCurrentSlide = (nextSlide) => {
     this.currentSlide = nextSlide;
+
     this.updateSlidePosition();
     this.updateIndicator();
   };
 
-  // 자동 슬라이드 전환
-  this.autoFlipSlide = (delayPlusTransitionSpeed = DEFAULT_SLIDE_SPEED) => {
+  // 슬라이드 자동 전환
+  this.autoFlipSlide = () => {
     this.clearAutoFlipSlide();
-    this.slideInterval = setInterval(() => {
-      this.setCurrentSlide((this.currentSlide + 1) % totalSlidesWithClone);
-    }, delayPlusTransitionSpeed);
+
+    // 슬라이드 전환이 끝나면 딜레이 후 다음 슬라이드로 이동
+    autoSlideTimeout = setTimeout(() => {
+      if (this.isPlaying)
+        this.setCurrentSlide((this.currentSlide + 1) % totalSlidesWithClone);
+    }, this.delay);
   };
 
-  // 타이머 초기화
-  this.clearAutoFlipSlide = () => clearInterval(this.slideInterval);
+  // 슬라이드 자동 전환 타이머 제거
+  this.clearAutoFlipSlide = () => {
+    if (autoSlideTimeout) {
+      clearTimeout(autoSlideTimeout);
+      autoSlideTimeout = null;
+    }
+  };
+
+  // 마지막 슬라이드에 걸린 타이머 제거
+  this.clearLastSlideTimeout = () => {
+    if (lastSlideTimeout) {
+      clearTimeout(lastSlideTimeout);
+      lastSlideTimeout = null;
+    }
+  };
 
   // 인디케이터 업데이트
   this.updateIndicator = () => {
@@ -70,8 +96,11 @@ export function Carousel({ slides, carouselSlide, delay, transitionSpeed }) {
       if (i === this.currentSlide) button.classList.add('selected');
 
       button.onclick = () => {
+        // 기존에 걸려있던 타이머 해제
+        this.clearAutoFlipSlide();
+        this.clearLastSlideTimeout();
+
         this.setCurrentSlide(i);
-        this.autoFlipSlide(delay + transitionSpeed);
       };
 
       indicatorContainer.appendChild(button);
@@ -88,14 +117,10 @@ export function Carousel({ slides, carouselSlide, delay, transitionSpeed }) {
 
   // 재생/일시정지 기능
   this.togglePlayPause = () => {
-    if (this.isPlaying) {
-      this.clearAutoFlipSlide();
-      this.playPauseButton.innerHTML = '▶';
-    } else {
-      this.autoFlipSlide(delay + transitionSpeed);
-      this.playPauseButton.innerHTML = '⏸';
-    }
     this.isPlaying = !this.isPlaying;
+    this.playPauseButton.innerHTML = this.isPlaying ? '⏸' : '▶';
+
+    if (this.isPlaying) this.autoFlipSlide();
   };
 
   // 네비게이션 버튼 생성
@@ -109,7 +134,9 @@ export function Carousel({ slides, carouselSlide, delay, transitionSpeed }) {
 
   // 슬라이드 이동
   this.navigateSlide = (direction) => {
+    // 타이머 해제
     this.clearAutoFlipSlide();
+    this.clearLastSlideTimeout();
 
     if (direction === 'prev') {
       if (this.currentSlide === 0) {
@@ -119,7 +146,7 @@ export function Carousel({ slides, carouselSlide, delay, transitionSpeed }) {
           (this.currentSlide * 100) / totalSlidesWithClone
         }%)`;
 
-        setTimeout(() => {
+        lastSlideTimeout = setTimeout(() => {
           carouselSlide.style.transition = `transform ${transitionSpeed}ms ease`;
           this.setCurrentSlide(totalSlides - 1);
         }, 300);
@@ -129,14 +156,12 @@ export function Carousel({ slides, carouselSlide, delay, transitionSpeed }) {
     } else {
       this.setCurrentSlide((this.currentSlide + 1) % totalSlidesWithClone);
     }
-
-    this.autoFlipSlide(delay + transitionSpeed);
   };
 
   // 슬라이드 생성
   this.render = () => {
     slides.forEach((slideSrc, index) => {
-      const slideHTML = /* html */ `
+      const slideHTML = `
         <div class="slide-container">
           <img
             class="slide-image"
@@ -150,11 +175,21 @@ export function Carousel({ slides, carouselSlide, delay, transitionSpeed }) {
 
     createIndicator();
     createNavigationButtons();
-    this.autoFlipSlide(delay + transitionSpeed);
+    this.autoFlipSlide();
   };
 
   this.render(); // 초기 렌더링
+
+  carouselSlide.addEventListener('transitionend', (event) => {
+    // 트랜지션이 끝나면 delay를 시작
+    if (event.propertyName === 'transform') this.autoFlipSlide();
+  });
+
   addDragAndTouchEventHandlers(carouselSlide, this.navigateSlide); // 터치 이벤트, 드래그 이벤트 등록
 
-  window.addEventListener('beforeunload', this.clearAutoFlipSlide); // 페이지가 닫힐 때 타이머 제거
+  // 페이지가 닫힐 때 타이머 제거
+  window.addEventListener('beforeunload', () => {
+    this.clearAutoFlipSlide();
+    this.clearLastSlideTimeout();
+  });
 }
